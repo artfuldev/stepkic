4e870ff5ab896618c1bf9899220eb96f2bea5001
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2024-01-13 11:08:32 +0000

    Show more engine details

--- src/renderer/game/players-view.tsx
+++ src/renderer/game/players-view.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { Player, Players } from "../../shared/model";
+import "./players-view.css";
 
 type Props = {
   players: Players;
@@ -7,12 +8,23 @@ type Props = {
 
 const name = Player.match({
   user: (name) => name,
-  engine: (_, { name }) => name,
+  engine: (_, { name, version }) => `${name} v${version}`,
 });
 
+const bio = Player.match({
+  user: () => "user",
+  engine: (_, { author }) => `by ${author}`
+})
+
 export const PlayersView: FC<Props> = ({ players }) => (
-  <aside className="players">
-    <div className="player x">{name(players.x)}</div>
-    <div className="player o">{name(players.o)}</div>
-  </aside>
+  <div style={{ display: "flex", flex: 1, justifyContent: "space-between" }}>
+    <div className="player x">
+      <div className="name">{name(players.x)}</div>
+      <div className="bio">{bio(players.x)}</div>
+    </div>
+    <div className="player o">
+      <div className="name">{name(players.o)}</div>
+      <div className="bio">{bio(players.x)}</div>
+    </div>
+  </div>
 );
