f3fac2122fec42999e78068398f659e6ff434049
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2024-01-13 10:09:10 +0000

    Allow customizing the winning length

--- src/shared/rules/wins.ts
+++ src/shared/rules/wins.ts
@@ -3,25 +3,33 @@ import { Rule } from "./rule";
 
 type Index = [number, number];
 
-const winning_positions = (board: Board): Position[][] => {
+const winning_positions = (board: Board, length?: number): Position[][] => {
+  if (length == null) length = board.length;
   const positions: Index[][] = [];
-  const size = board.length;
   const diagonal: Index[] = [];
   const anti_diagonal: Index[] = [];
+  const size = board.length;
+
   for (let i = 0; i < size; i++) {
-    const row: Index[] = [];
-    const column: Index[] = [];
-    for (let j = 0; j < size; j++) {
-      row.push([i, j]);
-      column.push([j, i]);
+    for (let j = 0; j <= size - length; j++) {
+      const row: Index[] = [];
+      const column: Index[] = [];
+      for (let k = 0; k < length; k++) {
+        row.push([i, j + k]);
+        column.push([j + k, i]);
+      }
+      positions.push(row);
+      positions.push(column);
     }
-    positions.push(row);
-    positions.push(column);
     diagonal.push([i, i]);
     anti_diagonal.push([i, size - i - 1]);
   }
-  positions.push(diagonal);
-  positions.push(anti_diagonal);
+
+  for (let i = 0; i <= size - length; i++) {
+    positions.push(diagonal.slice(i, i + length));
+    positions.push(anti_diagonal.slice(i, i + length));
+  }
+
   return positions.map((indices) =>
     indices.map(([x, y]) => Position.create(x, y))
   );
@@ -32,19 +40,17 @@ const win = (positions: Position[], board: Board): Side | undefined => {
   for (const position of positions) {
     const [row, column] = Position.indices(position);
     const cell = board[row]?.[column] ?? Cell.Playable();
-    if (cell.tag === "playable") return undefined;
-    if (cell.tag === "played") {
-      if (side == null) {
-        side = cell.args[0];
-        continue;
-      } else if (side !== cell.args[0]) return undefined;
-    }
+    if (cell.tag === "playable" || cell.tag === "unplayable") return undefined;
+    if (side == null) {
+      side = cell.args[0];
+      continue;
+    } else if (side !== cell.args[0]) return undefined;
   }
   return side;
 };
 
-export const wins = (_board: Board): Rule => {
-  const winners = winning_positions(_board);
+export const wins = (_board: Board, length?: number): Rule => {
+  const winners = winning_positions(_board, length);
   return (game) =>
     Game.match({
       started: (board) => {
