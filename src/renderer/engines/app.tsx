7885320c568e0dea9497ab9c78baf8b62c554cfd
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2024-01-28 08:30:23 +0000

    Extract out engine management with main

--- src/renderer/engines/app.tsx
+++ src/renderer/engines/app.tsx
@@ -1,40 +1,18 @@
-import React, { FC, useEffect, useState } from "react";
-import { EngineInfo, ProcessInfo } from "../../shared/model";
-import { Response } from "../../shared/messaging/engines/response";
-import { Request } from "../../shared/messaging/engines/request";
+import React, { FC, useState } from "react";
 import { EnginesView } from "./engines-view";
 import { AddEngine } from "./add-engine";
+import { useEngines } from "./use-engines";
 import "./global.scss";
 
 export const App: FC = () => {
-  const [engines, setEngines] = useState<[string, EngineInfo][]>([]);
-  const data = engines.map(([id, info]) => ({ ...info, id }));
-
-  useEffect(() => {
-    return window.electron.ipcRenderer.on(
-      "engines",
-      (_, response: Response) => {
-        setEngines(Object.entries(response.args[0]));
-      }
-    );
-  }, []);
-
-  useEffect(() => {
-    window.electron.ipcRenderer.send("engines", Request.List());
-  }, []);
-
-  const onDelete = (id: string) =>
-    window.electron.ipcRenderer.send("engines", Request.Delete(id));
-  const onAdd = (info: ProcessInfo) =>
-    window.electron.ipcRenderer.send("engines", Request.Create(info));
-
+  const { engines, onAdd, onDelete } = useEngines();
   const [open, setOpen] = useState(false);
 
   return (
     <>
       <AddEngine open={open} setOpen={setOpen} onAdd={onAdd} />
       <EnginesView
-        engines={data}
+        engines={engines}
         onAdd={() => setOpen(true)}
         onDelete={onDelete}
       />
---
+++ src/renderer/engines/use-engines.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { EngineInfo, ProcessInfo } from "../../shared/model";
+import { Response } from "../../shared/messaging/engines/response";
+import { Request } from "../../shared/messaging/engines/request";
+
+type IdentifiableEngine = EngineInfo & { id: string };
+
+type Result = {
+  engines: IdentifiableEngine[];
+  onAdd: (info: ProcessInfo) => void;
+  onDelete: (id: string) => void;
+};
+
+export const useEngines = (): Result => {
+  const [entries, setEntries] = useState<[string, EngineInfo][]>([]);
+  const engines = entries.map(([id, info]) => ({ ...info, id }));
+
+  useEffect(() => {
+    return window.electron.ipcRenderer.on(
+      "engines",
+      (_, response: Response) => {
+        setEntries(Object.entries(response.args[0]));
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    window.electron.ipcRenderer.send("engines", Request.List());
+  }, []);
+
+  const onDelete = (id: string) =>
+    window.electron.ipcRenderer.send("engines", Request.Delete(id));
+  const onAdd = (info: ProcessInfo) =>
+    window.electron.ipcRenderer.send("engines", Request.Create(info));
+
+  return {
+    engines,
+    onAdd,
+    onDelete
+  };
+};
