ae298145df407691894771f14117e3cc241b5618
Sudarsan Balaji <hello@artful.dev>
2024-10-03 23:34:08 +0100

    Simplify

--- src/main/engines/api.ts
+++ src/main/engines/api.ts
@@ -2,113 +2,42 @@
 import { ipcMain } from "electron";
 import { Request } from "../../shared/messaging/engines/request";
 import { Response } from "../../shared/messaging/engines/response";
-import { EngineInfo, Msvn, ProcessInfo } from "../../shared/model";
-import { spawn } from "child_process";
-import { createInterface } from "node:readline";
-import { Process } from "./process";
+import { Msvn, ProcessInfo } from "../../shared/model";
 import { sha1 } from "object-hash";
 import { Store } from "../store";
+import { Engine } from "./engine";
 
 export const api = (store: Store, msvn: Msvn) => {
-  const _delete = (id: string) => {
+  const remove = (id: string) => {
     const engines = store.get("engines", {});
     delete engines[id];
     store.set("engines", engines);
   };
-  const upsert = (
-    { cwd, command, args }: ProcessInfo,
-    next: (process: Process) => void
-  ) => {
-    let process = Process.Opened();
-    const { stdin, stdout, kill } = spawn(command, args, {
-      cwd,
-      stdio: ["pipe", "pipe", "pipe"],
-    });
-    const rl = createInterface({ input: stdout });
-    const matcher = (line: string) => {
-      console.log(process, line);
-      Process.match({
-        opened: () => {
-          if (line.trim() !== Msvn.expectation(msvn)) {
-            next(process);
-            return;
-          }
-          process = Process.Handshook({});
-          queueMicrotask(() => stdin.write("identify\n"));
-        },
-        handshook: (record) => {
-          if (!line.startsWith("identify ")) {
-            next(process);
-            return;
-          }
-          if (line.trim() === "identify ok") {
-            rl.off("line", matcher);
-            const keys = new Set(Object.keys(record));
-            if (
-              !keys.has("name") ||
-              !keys.has("version") ||
-              !keys.has("author") ||
-              !keys.has("url")
-            ) {
-              next(process);
-              return;
-            }
-            const engine = { cwd, command, args, ...record } as EngineInfo;
-            process = Process.Identified(engine);
-            stdin.write("quit\n");
-            const id = sha1(engine);
-            const engines = store.get("engines", {});
-            if (engines[id] != null) return;
-            engines[id] = engine;
-            store.set("engines", engines);
-            next(process);
-          }
-          const [key, value] = line.slice(9).split(" ");
-          process = Process.Handshook({ ...record, [key]: value });
-        },
-        identified: () => {
-          rl.off("line", matcher);
-          kill();
-        },
-      })(process);
-    };
-    rl.on("line", matcher);
-    stdin.write(`${Msvn.handshake(msvn)}\n`);
+
+  const add = async (processInfo: ProcessInfo) => {
+    const engine = new Engine(processInfo, msvn);
+    const engineIdentification = await engine.identify();
+    engine.kill();
+    const engineInfo = { ...processInfo, ...engineIdentification };
+    const id = sha1(engineInfo);
+    const engines = store.get("engines", {});
+    if (engines[id] != null) return;
+    engines[id] = engineInfo;
+    store.set("engines", engines);
   };
+
   ipcMain.on("engines", ({ reply }, request: Request) => {
     Request.match({
       list: () => {
-        const engines = store.get("engines", {});
-        console.log("engines", engines);
-        reply("engines", Response.Updated(engines));
+        reply("engines", Response.Updated(store.get("engines", {})));
       },
       create: (info) => {
-        upsert(
-          info,
-          Process.match({
-            opened: () => {},
-            handshook: () => {},
-            identified: () => {
-              reply("engines", Response.Updated(store.get("engines", {})));
-            },
-          })
-        );
-      },
-      update: (id, info) => {
-        upsert(
-          info,
-          Process.match({
-            opened: () => {},
-            handshook: () => {},
-            identified: () => {
-              _delete(id);
-              reply("engines", Response.Updated(store.get("engines", {})));
-            },
-          })
+        add(info).then(() =>
+          reply("engines", Response.Updated(store.get("engines", {})))
         );
       },
       delete: (id) => {
-        _delete(id);
+        remove(id);
         reply("engines", Response.Updated(store.get("engines", {})));
       },
     })(request);
--- src/shared/messaging/engines/request.ts
+++ src/shared/messaging/engines/request.ts
@@ -3,15 +3,13 @@ import { constructor, Tagged, Union, match } from "../../tagged";
 
 type List = Tagged<"list", []>;
 type Create = Tagged<"create", [ProcessInfo]>;
-type Update = Tagged<"update", [string, ProcessInfo]>;
 type Delete = Tagged<"delete", [string]>;
-type Variants = [List, Create, Update, Delete];
+type Variants = [List, Create, Delete];
 export type Request = Union<Variants>;
 
 export const Request = {
   List: constructor<Variants, List>("list"),
   Create: constructor<Variants, Create>("create"),
-  Update: constructor<Variants, Update>("update"),
   Delete: constructor<Variants, Delete>("delete"),
   match: match<Variants>(),
 };
