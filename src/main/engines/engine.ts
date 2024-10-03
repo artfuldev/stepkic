3a37900ca4357308814beb70f0a4007b04bb6dd3
Sudarsan Balaji <hello@artful.dev>
2024-10-03 23:34:02 +0100

    Add a simple engine abstraction

--- src/main/engines/engine.ts
+++ src/main/engines/engine.ts
@@ -8,65 +8,78 @@ import { nanoid } from "nanoid";
 const REQUIRED_KEYS = ["name", "version", "author", "url"];
 
 export class Engine {
-  private readonly _log: Debugger;
-  private readonly _stdin: Writable;
-  private readonly _kill: () => void;
-  private readonly _rl: Interface;
-  private _initialized = false;
+  readonly #log: Debugger;
+  readonly #stdin: Writable;
+  readonly #kill: () => void;
+  readonly #rl: Interface;
 
-  constructor({ cwd, command, args }: ProcessInfo, private readonly msvn: Msvn) {
+  constructor(
+    { cwd, command, args }: ProcessInfo,
+    private readonly msvn: Msvn
+  ) {
     const { stdin, stdout, kill } = spawn(command, args, {
       cwd,
       stdio: ["pipe", "pipe", "pipe"],
     });
-    this._stdin = stdin;
-    this._log = debug("engine").extend(nanoid());
-    this._rl = createInterface({ input: stdout });
-    this._kill = kill;
-    this._log("started process");
+    this.#stdin = stdin;
+    this.#log = debug("engine").extend(nanoid());
+    this.#rl = createInterface({ input: stdout });
+    this.#kill = kill;
+    this.#log("started process");
   }
 
-  async initialize() {
+  #in(listener: (line: string) => void) {
+    return (line: string) => {
+      this.#log("< %s", line);
+      listener(line);
+    };
+  }
+
+  #out(line: string) {
+    this.#log("> %s", line);
+    this.#stdin.write(`${line}\n`);
+  }
+
+  async #initialize() {
     return new Promise<void>((resolve, reject) => {
-      this._rl.once("line", (answer) => {
-        this._log('< %s', answer);
-        if (answer.trim() === Msvn.expectation(this.msvn)) {
-          this._initialized = true;
-          resolve();
-        }
-        else reject(new Error(`invalid handshake: ${answer}`));
-      });
-      const command = Msvn.handshake(this.msvn);
-      this._log('> %s', command);
-      this._stdin.write(`${command}\n`);
+      this.#rl.once(
+        "line",
+        this.#in((answer) => {
+          if (answer.trim() === Msvn.expectation(this.msvn)) {
+            resolve();
+          } else reject(new Error(`invalid handshake: ${answer}`));
+        })
+      );
+      this.#out(Msvn.handshake(this.msvn));
     });
   }
 
   async identify() {
+    await this.#initialize();
     return new Promise<EngineIdentification>((resolve, reject) => {
       const map = new Map();
-      const listener = (line: string) => {
-        this._log('< %s', line);
+      const listener = this.#in((line: string) => {
         const trimmed = line.trim();
-        if (!trimmed.startsWith('identify ')) return;
+        if (!trimmed.startsWith("identify ")) return;
         const pruned = trimmed.slice(9);
-        if (pruned === 'ok') {
-          this._rl.off('line', listener);
-          const missing = REQUIRED_KEYS.filter(x => !map.has(x));
-          if (missing.length !== 0) reject(new Error(`missing keys in identify: ${missing}`));
+        if (pruned === "ok") {
+          this.#rl.off("line", listener);
+          const missing = REQUIRED_KEYS.filter((x) => !map.has(x));
+          if (missing.length !== 0)
+            reject(new Error(`missing keys in identify: ${missing}`));
           else resolve(Object.fromEntries(map.entries()));
         }
         const [key, value] = pruned.split(" ");
-        map.set(key, value)
-      }
-      this._rl.on('line', listener);
-      const command = 'identify';
-      this._log('> %s', command);
-      this._stdin.write(`${command}\n`);
+        map.set(key, value);
+      });
+      this.#rl.on("line", listener);
+      this.#out("identify");
     });
   }
 
   kill() {
-    this._kill();
+    this.#log("kill requested");
+    this.#kill();
+    this.#log("killed");
   }
 }
