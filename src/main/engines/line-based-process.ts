19ca0be966fa8b3c3d1213973f277509063faf97
Sudarsan Balaji <hello@artful.dev>
2024-10-05 12:28:47 +0100

    Use a single logger for process

--- src/main/engines/line-based-process.ts
+++ src/main/engines/line-based-process.ts
@@ -7,7 +7,6 @@ import { Observable, Subject } from "rxjs";
 export class LineBasedProcess {
   readonly #process: ChildProcessWithoutNullStreams;
   readonly #log: Debugger;
-  readonly #out: Debugger;
   readonly #rl: Interface;
   readonly lines: Observable<string>;
   constructor({ cwd, command, args }: ProcessInfo, log: Debugger) {
@@ -18,17 +17,15 @@ export class LineBasedProcess {
     });
     this.#log("started process");
     this.#rl = createInterface({ input: this.#process.stdout });
-    const _log = log.extend("<");
-    this.#out = log.extend(">");
     const subject = new Subject<string>();
-    this.#rl.on("line", _log.bind(_log));
+    this.#rl.on("line", line => this.#log('<: %s', line));
     this.#rl.on("line", (line) => subject.next(line.trim()));
     this.lines = subject.asObservable();
     this.#log("started observing input stream");
   }
 
   send(line: string) {
-    this.#out(line);
+    this.#log('>: %s', line);
     this.#process.stdin.write(`${line}\n`);
     return this;
   }
